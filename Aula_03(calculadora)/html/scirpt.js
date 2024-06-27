5a3e17b25fe833bc5a2b439ab19f0ded8d5a72bd
Erick Gusson <80285372+erickgusson@users.noreply.github.com>
2024-06-27 10:38:48 -0300

    Pequenos avancos

--- Aula_03(calculadora)/html/scirpt.js
+++ Aula_03(calculadora)/html/scirpt.js
@@ -1,4 +1,37 @@
 function Calcular() {
-    let teste = document.getElementById('opcao').value;
-    document.getElementById('resultado').innerHTML  = teste;
+    var resultado = 0;
+    let n1 = parseFloat(document.getElementById('n1').value);
+    let n2 = parseFloat(document.getElementById('n2').value);
+    let opcao = document.getElementById('opcao').value;
+    if (opcao != 'nenhum') {
+        Conta(opcao);
+    } else {
+        alert('Selecione uma opção para a conta');
+    }
+}
+
+function Conta(opcao) {
+    if (opcao == 'soma') {
+        resultado = n1 + n2;
+        Exibir(resultado);
+    }
+    else if (opcao == 'subtracao') {
+        resultado = n1 - n2;
+        Exibir(resultado);
+    }
+    else if (opcao == 'multiplicacao') {
+        resultado = n1 * n2;
+        Exibir(resultado);
+    }
+    else if (opcao == 'divisao') {
+        resultado = n1 / n2;
+        Exibir(resultado);
+    }
+    else {
+
+    }
+}
+
+function Exibir(resultado) {
+    document.getElementById('resultado').innerHTML = resultado;
 }
